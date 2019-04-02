5838f64b8252504b899bb3a4ca67d625238f114e
derui <derutakayu@gmail.com>
2019-04-02 21:57:19 +0900

    fix: apply answer fro abc002/D

--- atcoder/abc002/D/main.ml
+++ atcoder/abc002/D/main.ml
@@ -1,11 +1,11 @@
 let make_matrix n = Array.init n (fun _ -> Array.make n false)
 
 let loop f accum count =
-  let rec inner_loop accum current =
-    if current >= count then accum
+  let rec inner_loop accum' current =
+    if current >= count then accum'
     else
-      let accum = f accum current in
-      inner_loop accum (succ current)
+      let accum' = f accum' current in
+      inner_loop accum' (succ current)
   in
   inner_loop accum 0
 
@@ -13,35 +13,30 @@ let count_max_relations matrix =
   let size = Array.length matrix in
   let has_bit pat v = (pat lsr v) land 1 = 1 in
   let bit_count v =
-    let rec bit_count v accum =
-      if v = 0 then accum else bit_count (v lsr 1) (succ accum)
+    let rec _bit_count v accum =
+      if v = 0 then accum
+      else if v land 1 = 0 then _bit_count (v lsr 1) accum
+      else _bit_count (v lsr 1) (succ accum)
     in
-    bit_count v 0
+    _bit_count v 0
   in
   loop
     (fun accum current_pattern ->
-       Printf.printf "pat: %d, accum %d\n" current_pattern accum ;
        let count = bit_count current_pattern in
        if accum >= count then accum
        else
          let ret =
            loop
-             (fun _ i ->
+             (fun accum i ->
                 loop
                   (fun accum j ->
-                     Printf.printf
-                       "pat: %d: i: %d, j: %d, bit(i): %b, bit(j): %b, matrix: %b\n"
-                       current_pattern i j
-                       (has_bit current_pattern i)
-                       (has_bit current_pattern j)
-                       matrix.(i).(j) ;
                      if
                        has_bit current_pattern i && has_bit current_pattern j
                        && not matrix.(i).(j)
                      then false
                      else accum )
-                  true i )
-             false size
+                  accum i )
+             true size
          in
          if ret then count else accum )
     0 (1 lsl size)
