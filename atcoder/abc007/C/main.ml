494bb4c29c0f5f01be17aadfc449875d993d44a6
derui <derutakayu@gmail.com>
2019-04-29 20:10:26 +0900

    add abc007/C

--- atcoder/abc007/C/main.ml
+++ atcoder/abc007/C/main.ml
@@ -3,8 +3,10 @@ let take_list ~num list =
     if count = 0 then List.rev accum
     else
       match rest with
-      | [] -> List.rev accum
-      | v :: rest -> take_list' (pred count) rest (v :: accum)
+      | [] ->
+          List.rev accum
+      | v :: rest ->
+          take_list' (pred count) rest (v :: accum)
   in
   take_list' num list []
 
@@ -36,6 +38,74 @@ let combination n m =
   let n' = factorial n and m' = factorial m and nm' = factorial (n - m) in
   Big_int.(div_big_int n' (mult_big_int m' nm'))
 
+type block = Free | Wall
+
+let read_maze rows =
+  let rec read' current accm =
+    if current >= rows then List.rev accm
+    else
+      let row = ref [] in
+      let line = read_line () in
+      String.iter
+        (function
+          | '.' ->
+              row := Free :: !row
+          | '#' ->
+              row := Wall :: !row
+          | _ ->
+              failwith "unknown character" )
+        line ;
+      read' (succ current) (List.rev !row :: accm)
+  in
+  read' 0 []
+
+let around_frees maze ~x ~y =
+  let arounds = [(x + 1, y); (x - 1, y); (x, y + 1); (x, y - 1)] in
+  List.map (fun (x, y) -> (x, y, List.nth (List.nth maze y) x)) arounds
+  |> List.filter (function _, _, Free -> true | _, _, _ -> false)
+
+module Hand_map = Map.Make (struct
+  type t = int * int
+
+  let compare = compare
+end)
+
+let solve ~maze ~rows ~columns ~start:(sx, sy) ~goal:(gx, gy) =
+  let queue : (int * int) Queue.t = Queue.create () in
+  let hand_map : int Hand_map.t = Hand_map.empty in
+  let rec solve' hand_map =
+    if Queue.is_empty queue then hand_map
+    else
+      let x, y = Queue.take queue in
+      let arounds = around_frees maze ~x ~y in
+      let cell_hand = Hand_map.find (x, y) hand_map in
+      let hand_map =
+        List.fold_left
+          (fun map (x, y, _) ->
+            if not (Hand_map.mem (x, y) map) then (
+              Queue.push (x, y) queue ;
+              Hand_map.add (x, y) (succ cell_hand) map )
+            else map )
+          hand_map arounds
+      in
+      solve' hand_map
+  in
+  let hand_map = Hand_map.add (sx, sy) 0 hand_map in
+  Queue.add (sx, sy) queue ;
+  solve' hand_map |> Hand_map.find (gx, gy)
 
 let () =
-  (* need implementation *)
+  let rows, columns =
+    Scanf.sscanf (read_line ()) "%d %d" (fun v1 v2 -> (v1, v2))
+  in
+  let starty, startx =
+    Scanf.sscanf (read_line ()) "%d %d" (fun v1 v2 -> (pred v1, pred v2))
+  in
+  let goaly, goalx =
+    Scanf.sscanf (read_line ()) "%d %d" (fun v1 v2 -> (pred v1, pred v2))
+  in
+  let maze = read_maze rows in
+  let result =
+    solve ~maze ~rows ~columns ~start:(startx, starty) ~goal:(goalx, goaly)
+  in
+  Printf.printf "%d\n" result
