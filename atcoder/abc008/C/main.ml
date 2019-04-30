0506a8e23b5997ad0c28b9489501ad2448c8bc93
derui <derutakayu@gmail.com>
2019-05-01 08:54:29 +0900

    feat: add abc008/C solves

--- atcoder/abc008/C/main.ml
+++ atcoder/abc008/C/main.ml
@@ -3,8 +3,10 @@ let take_list ~num list =
     if count = 0 then List.rev accum
     else
       match rest with
-      | [] -> List.rev accum
-      | v :: rest -> take_list' (pred count) rest (v :: accum)
+      | [] ->
+          List.rev accum
+      | v :: rest ->
+          take_list' (pred count) rest (v :: accum)
   in
   take_list' num list []
 
@@ -36,6 +38,27 @@ let combination n m =
   let n' = factorial n and m' = factorial m and nm' = factorial (n - m) in
   Big_int.(div_big_int n' (mult_big_int m' nm'))
 
+let read_lines count ~f =
+  let rec read_lines' current accum =
+    if current >= count then List.rev accum
+    else
+      let line = read_line () |> f in
+      read_lines' (succ current) (line :: accum)
+  in
+  read_lines' 0 []
+
+let solve coins =
+  let correct_divisors v = List.filter (fun v' -> Int64.rem v v' = 0L) coins in
+  let calculate_expected v =
+    let divisors = (correct_divisors v |> List.length) - 1 in
+    let divisors' = float_of_int divisors in
+    if divisors mod 2 = 1 then 0.5
+    else (divisors' +. 2.) /. ((2. *. divisors') +. 2.)
+  in
+  List.fold_left (fun accum v -> accum +. calculate_expected v) 0. coins
 
 let () =
-  (* need implementation *)
+  let count = read_line () |> int_of_string in
+  let coins = read_lines count ~f:Int64.of_string in
+  let result = solve coins in
+  Printf.printf "%.6f\n" result
